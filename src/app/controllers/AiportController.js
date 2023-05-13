89c9004d9c8de44206c92974b7ea706856875a06
Luís Silva <gabrielima.si@gmail.com>
2023-05-13 16:07:40 -0300

    Add logic to find all airports

--- src/app/controllers/AiportController.js
+++ src/app/controllers/AiportController.js
@@ -2,13 +2,30 @@ const db = require('../db/conn');
 
 // MODELS
 const airports = db.airports;
+const cities = db.cities;
 
 class AirportController {
   async listAll(req, res) {
     try {
-      res.status(200).json({
-        msg: `Teste`,
+      const allAirports = await airports.findAll({
+        attributes: { exclude: ['city_id'] },
+        include: [
+          {
+            model: cities,
+            attributes: ['name', 'uf'],
+          },
+        ],
       });
+
+      if (Object.keys(allAirports).length === 0) {
+        res.status(404).json({
+          msg: 'Não foram encontrados aeroportos cadastrados!',
+        });
+      } else {
+        res.status(200).json({
+          airports: allAirports,
+        });
+      }
     } catch (error) {
       console.log(error);
       res.status(500);
