f9685932ca5bb14a85e0f4b1afda175968c72554
Luís Silva <gabrielima.si@gmail.com>
2023-05-14 11:02:24 -0300

    Added createFlight controller

---
+++ src/app/controllers/FlightController.js
@@ -0,0 +1,45 @@
+const db = require('../db/conn');
+
+// MODELS
+const Airports = db.airports;
+const Cities = db.cities;
+const Flights = db.flights;
+const FlightClasses = db.flight_classes;
+
+class FlightController {
+  async createFlight(req, res) {
+    try {
+      const { departure_time, origin_id, destination_id, classes } = req.body;
+
+      // Creating flight info
+      const flight = await Flights.create({
+        flight_number: 'Teste',
+        departure_time,
+        origin_id,
+        destination_id,
+      });
+
+      // Creating classes info and association
+      for (let i = 0; i < classes.length; i++) {
+        let { type, name, qty_of_seats, price_per_seat } = classes[i];
+
+        await FlightClasses.create({
+          type,
+          name,
+          qty_of_seats,
+          price_per_seat,
+          flight_id: flight.id,
+        });
+      }
+
+      res.status(200).json({ msg: 'ok' });
+      return;
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+      return;
+    }
+  }
+}
+
+module.exports = new FlightController();
