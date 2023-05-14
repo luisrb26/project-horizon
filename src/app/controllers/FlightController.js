a6b96c445af42fb330221c8ab4545f644d872c86
Luís Silva <gabrielima.si@gmail.com>
2023-05-14 18:02:37 -0300

    Added ability of creating seats in FlightController

--- src/app/controllers/FlightController.js
+++ src/app/controllers/FlightController.js
@@ -1,10 +1,9 @@
 const db = require('../db/conn');
 
 // MODELS
-const Airports = db.airports;
-const Cities = db.cities;
 const Flights = db.flights;
 const FlightClasses = db.flight_classes;
+const Seats = db.seats;
 
 class FlightController {
   async createFlight(req, res) {
@@ -19,19 +18,39 @@ class FlightController {
         destination_id,
       });
 
-      // Creating classes info and association
       for (let i = 0; i < classes.length; i++) {
         let { type, name, qty_of_seats, price_per_seat } = classes[i];
-
-        await FlightClasses.create({
+        // Creating classes info and association
+        const flightClass = await FlightClasses.create({
           type,
           name,
           qty_of_seats,
           price_per_seat,
           flight_id: flight.id,
         });
-      }
 
+        // Generating seats name
+        let seatsNames = [];
+        for (let i = 1; i <= qty_of_seats; i++) {
+          const number = i.toString();
+          seatsNames.push(type + number);
+        }
+
+        // Bulk creating seats info
+        await Seats.bulkCreate(
+          // Creates a array of objects with attribute seat_name
+          seatsNames.map((seatName) => ({
+            seat_name: seatName,
+            flight_class_id: flightClass.id,
+          }))
+        )
+          .then(() => {
+            console.log('ok');
+          })
+          .catch((error) => {
+            console.error('Error', error);
+          });
+      }
       res.status(200).json({ msg: 'ok' });
       return;
     } catch (error) {
