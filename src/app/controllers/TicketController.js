452a1dd84bd58e004e8ddeffaeae3ec88600b5c8
Luís Silva <gabrielima.si@gmail.com>
2023-05-15 14:20:24 -0300

    Added a function to set a time when searching today tickets

--- src/app/controllers/TicketController.js
+++ src/app/controllers/TicketController.js
@@ -1,3 +1,4 @@
+const { format, isSameDay, parseISO } = require('date-fns');
 const { Op } = require('sequelize');
 const db = require('../db/conn');
 
@@ -10,25 +11,32 @@ class TicketController {
     try {
       const { origin_id, destination_id, date } = req.body;
 
+      const today = new Date();
+      let currentDateTime = parseISO(date);
+
+      if (isSameDay(currentDateTime, today)) {
+        currentDateTime = format(today, "yyyy-MM-dd'T'HH:mm:ss.SSS'-0300'");
+      }
+
       const flightsFound = await flights.findAll({
+        where: {
+          origin_id,
+          destination_id,
+          status: 1, // Active Status
+          seats_availible: {
+            [Op.gt]: 0,
+          },
+          departure_time: {
+            [Op.gte]: new Date(`${currentDateTime}`),
+            [Op.lte]: new Date(`${date} 23:59:59`),
+          },
+        },
         include: [
           {
             model: flightsClasses,
             attributes: ['name', 'seats_availible', 'price_per_seat'],
           },
         ],
-        where: {
-          origin_id,
-          destination_id,
-          status: 1,
-          seats_availible: {
-            [Op.gt]: 0,
-          },
-          departure_time: {
-            [Op.gte]: new Date(`${date}`),
-            [Op.lte]: new Date(`${date} 23:59:59`),
-          },
-        },
       });
 
       res.status(200).json({ tickets: flightsFound });
