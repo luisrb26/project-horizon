45eae5c4c1fe08394cd05fafd952cc5bd6cf9c17
Luís Silva <gabrielima.si@gmail.com>
2023-05-15 19:04:22 -0300

    Added a buy ticket validation

--- src/app/routes/ticketRoutes.js
+++ src/app/routes/ticketRoutes.js
@@ -4,6 +4,6 @@ const validate = require('../validations/ticketValidations');
 const TicketController = require('../controllers/TicketController');
 
 router.get('/find', validate.findTickets, TicketController.findTickets);
-router.post('/buy', TicketController.buyTickets);
+router.post('/buy', validate.buyTickets, TicketController.buyTickets);
 
 module.exports = router;
--- src/app/validations/ticketValidations.js
+++ src/app/validations/ticketValidations.js
@@ -1,5 +1,6 @@
 const { body, validationResult } = require('express-validator');
 const { isAfter, isValid, isSameDay, parseISO } = require('date-fns');
+const { validate } = require('gerador-validador-cpf');
 
 exports.findTickets = [
   body('origin_id')
@@ -34,6 +35,59 @@ exports.findTickets = [
       }
       return true;
     }),
+  (req, res, next) => {
+    // Find for validations erros and store then in an array
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else next();
+  },
+];
+
+exports.buyTickets = [
+  body('buyers')
+    .isArray({ min: 1 })
+    .withMessage('Buyers must be an array with at least one element'),
+  body('buyers.*.first_name')
+    .isString()
+    .notEmpty()
+    .withMessage('First name is required'),
+  body('buyers.*.last_name')
+    .isString()
+    .notEmpty()
+    .withMessage('Last name is required'),
+  body('buyers.*.email').isEmail().withMessage('Invalid email address'),
+  body('buyers.*.cpf')
+    .notEmpty()
+    .withMessage('CPF is required')
+    .isLength({ min: 11, max: 11 })
+    .withMessage('CPF must have 11 digits')
+    .matches(/^[0-9]+$/)
+    .withMessage('CPF must contain only digits')
+    .custom((value) => {
+      if (!validate(value)) {
+        throw new Error('Invalid CPF');
+      }
+      return true;
+    }),
+  body('buyers.*.birth_date').isISO8601().withMessage('Invalid birth date'),
+  body('buyers.*.sex').isString().notEmpty().withMessage('Sex is required'),
+  body('buyers.*.tickets')
+    .isArray({ min: 1 })
+    .withMessage('Tickets must be an array with at least one element'),
+  body('buyers.*.tickets.*.flight_id')
+    .isInt({ min: 1 })
+    .withMessage('Flight ID must be a positive integer'),
+  body('buyers.*.tickets.*.flight_class_id')
+    .isInt({ min: 1 })
+    .withMessage('Flight class ID must be a positive integer'),
+  body('buyers.*.tickets.*.baggage')
+    .optional()
+    .isInt({ min: 0, max: 2 })
+    .withMessage('Baggage must be an integer between 0 and 2'),
+  body('buyers.*.tickets.*.seat')
+    .isInt({ min: 1 })
+    .withMessage('Seat ID must be a positive integer'),
   (req, res, next) => {
     // Find for validations erros and store then in an array
     const errors = validationResult(req);
