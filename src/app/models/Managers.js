895f84fb6e8176c344944351859b2735eddde1dc
Luís Silva <gabrielima.si@gmail.com>
2023-05-15 19:29:15 -0300

    Added Managers table

---
+++ src/app/models/Managers.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Managers = sequelize.define('managers', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      unique: true,
+    },
+    username: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+  });
+  return Managers;
+};
